b7ed7b8895918c2355032f428f2f7e814136df7b
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-01-09 15:17:06 +0100

    script

---
+++ main.js
@@ -0,0 +1,62 @@
+// Advice array
+const advice = ["Walcz!", "Przemyśl to jeszcze raz..."];
+
+// 'Add' & 'reset' variables
+const input = document.querySelector(`input`);
+const addBtn = document.querySelector(`.add`);
+const cleanBtn = document.querySelector(`.clean`);
+
+// 'Add' function
+const addAdvice = (e) => {
+  e.preventDefault();
+
+  if (input.value.length) {
+    for (const adv of advice) {
+      if (adv === input.value) {
+        alert("To już jest!");
+        return;
+      }
+    }
+
+    advice.push(input.value);
+    input.value = "";
+  } else {
+    alert("Musisz coś wpisać...");
+  }
+
+  console.log(advice);
+};
+
+// 'Add' listener
+addBtn.addEventListener(`click`, addAdvice);
+
+// 'Reset' function
+const cleanAdvice = (e) => {
+  e.preventDefault();
+  advice.length = 0;
+};
+
+// 'Reset' listener
+cleanBtn.addEventListener(`click`, cleanAdvice);
+
+// 'Show' & 'options' variables
+const showAdviceBtn = document.querySelector(`.showAdvice`);
+const showOptionsBtn = document.querySelector(`.showOptions`);
+const h1 = document.querySelector(`h1`);
+
+// 'Show' function
+const showAdvice = () => {
+  const random = Math.floor(Math.random() * advice.length);
+  h1.textContent = advice[random];
+};
+
+// 'Show' listener
+showAdviceBtn.addEventListener(`click`, showAdvice);
+
+// 'Options' function
+const showOptions = () => {
+  alert(advice);
+};
+
+// 'Options' listener
+showOptionsBtn.addEventListener(`click`, showOptions);
